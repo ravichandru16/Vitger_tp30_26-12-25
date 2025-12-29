5a0f3fc5b54e0351d45e9f6903589600334fef38
hp <hp@DESKTOP-DJ28UVR>
2025-12-29 10:57:28 +0530

    new one

--- tp30_Github/src/test/java/leadsModule/practice_1.java
+++ tp30_Github/src/test/java/leadsModule/practice_1.java
@@ -3,7 +3,8 @@ package leadsModule;
 public class practice_1 {
 public static void main(String[] args) {
 	
-	System.out.println("----hello----");
+	System.out.println("----hello----"); 
+	System.out.println("----hello----"); 
 }
 
 }
