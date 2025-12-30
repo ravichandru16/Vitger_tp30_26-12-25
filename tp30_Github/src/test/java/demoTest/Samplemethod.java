3a0adbe54a51b4e3ffb716ee395a7dfa80eef59a
ravi1 <ravi1@LAPTOP-4QN7VMLV>
2025-12-30 15:50:32 +0530

    hiii

---
+++ tp30_Github/src/test/java/demoTest/Samplemethod.java
@@ -0,0 +1,19 @@
+package demoTest;
+
+import org.testng.annotations.Test;
+
+import genericUtility.BaseClass_Utilities;
+
+public class Samplemethod extends BaseClass_Utilities {
+	@Test
+	public void method1()
+	{
+	System.out.println("method1");	
+	}
+	
+	@Test
+	public void method2()
+	{
+		System.out.println("method 2");
+	}
+}
